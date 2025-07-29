a8d13bd2452447b8a319b1fbe2e2e3909076ecf8
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2025-07-29 01:24:46 -0300

    feat: add routes react-router-dom

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { FacebookLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from '@phosphor-icons/react'
 
 function Footer() {
 
@@ -9,13 +9,19 @@ function Footer() {
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                            Blog Pessoal Generation | Copyright: {data}
-                        </p>
+                        Blog Pessoal Generation | Copyright: {data}
+                    </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                        <FacebookLogo size={48} weight='bold' />
+                        <a href="https://www.linkedin.com/in/diegoscavalcanti" target="_blank">
+                            <LinkedinLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.instagram.com/diego.cavalcantti" target="_blank">
+                            <InstagramLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.facebook.com/diegocavalcantis" target="_blank">
+                            <FacebookLogoIcon size={48} weight='bold' />
+                        </a>
                     </div>
                 </div>
             </div>
