8ae06d1271db58c16af7ebb50ad484978001cb9b
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2025-07-31 14:17:56 -0300

    feat: crud temas blog

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,15 @@ import Home from './pages/home/Home'
 import './App.css'
 import Cadastro from './pages/cadastro/Cadastro'
 import Login from './pages/login/Login'
+import { AuthProvider } from './contexts/AuthContext'
+import ListaTemas from './components/temas/listaTemas/ListaTemas'
+import FormTema from './components/temas/formTema/FormTema'
+import DeletarTema from './components/temas/deletarTema/DeletarTema'
 
 function App() {
   return (
     <>
+      <AuthProvider>
         <BrowserRouter>
           <Navbar />
           <div className="min-h-[80vh]">
@@ -17,10 +22,15 @@ function App() {
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
+              <Route path='temas' element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
